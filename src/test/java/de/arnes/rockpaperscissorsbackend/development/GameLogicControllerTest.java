7653e7b2789358f1d7d20a0b01f8a7ee548a830d
Arne <25064755+derArneS@users.noreply.github.com>
2022-10-14 16:38:47 +0200

    New test for method createPlayResponse

---
+++ src/test/java/de/arnes/rockpaperscissorsbackend/development/GameLogicControllerTest.java
@@ -0,0 +1,42 @@
+package de.arnes.rockpaperscissorsbackend.development;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import org.junit.Test;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import de.arnes.rockpaperscissorsbackend.model.game.GameResponse;
+import de.arnes.rockpaperscissorsbackend.model.game.Result;
+import de.arnes.rockpaperscissorsbackend.model.game.Shape;
+import de.arnes.rockpaperscissorsbackend.rest.controller.GameLogicController;
+
+/**
+ * 
+ * @author Arne S.
+ *
+ */
+public class GameLogicControllerTest {
+
+	private GameLogicController testee = new GameLogicController();
+
+	/**
+	 * Checks if the method createPlayResponse generates the correct
+	 * {@link EntityModel}.
+	 */
+	@Test
+	public void createPlayResponseShouldReturnCorrectEntityModel() {
+		EntityModel<GameResponse> expected = EntityModel.of(new GameResponse(Result.DRAW),
+				linkTo(methodOn(GameLogicController.class).play(Shape.ROCK, Shape.ROCK)).withSelfRel(), //
+				linkTo(methodOn(GameLogicController.class).computer()).withRel("computer"));
+
+		// Method needs reflection because it is private.
+		EntityModel<GameResponse> actual = ReflectionTestUtils.invokeMethod(testee, "createPlayResponse", Shape.ROCK,
+				Shape.ROCK, Result.DRAW);
+
+		assertEquals(expected, actual);
+	}
+
+}
